f9db6181d53cc2a19eca256f18825e043ea7fc31
JanEuster <gh@jan.eusterschulte.com>
2022-07-04 14:37:29 +0200

    cyceling placeholder + styles results field

--- components/Dashboard/Converter/converter.styles.tsx
+++ components/Dashboard/Converter/converter.styles.tsx
@@ -9,7 +9,7 @@ export const HeaderTitle = styled(NoteHeaderTitle)`
   font-size: 24px;
 `;
 
-export const Wrapper = styled.div`
+export const ContentWrapper = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
@@ -23,8 +23,35 @@ export const Input = styled.input`
   height: 36px;
   padding: 0 5px;
   font-size: 18px;
+  background: var(--two);
+  border: none;
+  color: var(--text);
+
+  &:focus {
+    outline: 2px solid var(--four);
+  }
+
+  &::-webkit-input-placeholder {
+    opacity: 0.4;
+  }
 `
 
 export const Result = styled.p`
+  font-family: Roboto;
+  font-size: 20px;
+  outline: 1px solid var(--six);
+  padding: 10px 30px;
+  max-width: 90%;
+  overflow-wrap: anywhere;
 
+  position: relative;
+  &::before {
+    position: absolute;
+    content: "Result:";
+    top: 0;
+    left: 0;
+    font-size: 14px;
+    font-weight: 700;
+    transform: translateY(-100%);
+  }
 `
--- components/Dashboard/Converter/index.tsx
+++ components/Dashboard/Converter/index.tsx
@@ -1,27 +1,48 @@
 import { create, all } from "mathjs";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { IFlex } from "../../../types"
 import { AppContainerVH, HL } from "../../common";
-import { Header, HeaderTitle, Input, Result, Wrapper } from "./converter.styles";
+import { Header, HeaderTitle, Input, Result, ContentWrapper } from "./converter.styles";
 
 const math = create(all);
 
 const ConverterApp: React.FC<IFlex> = ({ basis, shrink, grow }) => {
   // const converterCategories = ["Length", "Area", "Volume", "Weight", "Currency"]
   const [result, setResult] = useState<string>("");
+  const [placeholder, setPlaceholder] = useState<number>(0)
+  const [tex, setTex] = useState<string>("\\(\\frac{10}{4x} \\approx 2^{12}\\)");
+  const placeholders = ["10 lbs to kg", "1l to gallon", "2^10", "pi rad to deg", "(242 * 11.5) / 2"]
 
   const evalOnChange = (e: ChangeEvent<HTMLInputElement>) => {
     try {
       let res = math.evaluate(e.target.value);
       setResult(String(res))
+
+      const node = math.parse(e.target.value);
+
+      setTex(node.toString());
     }
     catch (err: any) {
-      if (!err.message == undefined) {
-        setResult(String(err.message))
-      }
+      setResult(String(err.message))
+      setTex("");
     }
 
   }
+
+  // cycle placeholders
+  useEffect(() => {
+    let placeholderInterval = setInterval(() => {
+      if (placeholder + 1 < placeholders.length) {
+        setPlaceholder(placeholder + 1);
+      } else {
+        setPlaceholder(0);
+      }
+    }, 5 * 1000);
+
+    return () => {
+      clearInterval(placeholderInterval);
+    }
+  })
 
   return (
     <AppContainerVH basis={basis} shrink={shrink} grow={grow}>
@@ -29,10 +50,10 @@ const ConverterApp: React.FC<IFlex> = ({ basis, shrink, grow }) => {
         <HeaderTitle>Unit Converter / Calculator</HeaderTitle>
       </Header>
       <HL></HL>
-      <Wrapper>
-        <Input placeholder="10 lbs to kg" onChange={evalOnChange}></Input>
+      <ContentWrapper>
+        <Input placeholder={placeholders[placeholder]} onChange={evalOnChange}></Input>
         <Result>{result}</Result>
-      </Wrapper>
+      </ContentWrapper>
     </AppContainerVH >
   )
 }
